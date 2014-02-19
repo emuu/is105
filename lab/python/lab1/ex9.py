62e05485c6b3d7ac7ce5c3c2cc36a7c8527c261d
pjfedog <julianwf@yahoo.no>
2014-02-19 11:24:22 +0100

    endret mappestruktur

---
+++ lab/python/lab1/ex9.py
@@ -0,0 +1,16 @@
+#her definierer du variablen "days"
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#her definerer du variablen "months"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#her sier du at variablen days skal printes
+print "Here are the days: ", days
+#her sier du at variablen months skal printers
+print "Here are the months: ", months
+
+#her viser du at du kan skrive sa mye du vil nar du lager 3 anforselstegn
+print """
+There's something going on here.
+With th three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
